047cd512e51aa2aad5e2549c6018d5c4d4f2f508
Gael Touquet <gael.touquet@gmail.com>
2022-11-07 09:28:35 +0100

    formatting

--- library.sql
+++ library.sql
@@ -21,35 +21,35 @@ CREATE TABLE borrow (
 -- test case, dummy values
 INSERT INTO book (name)
 VALUES ('Dune'),
-('Lanfeust'),
-('Harry Potter'),
-('Lhistoire pour les nuls'),
-('LOTR'),
-('La vie est belle'), 
-('La valse lente des tortues'), 
-('Moi christiane F.'), 
-('SQL pour les nuls'), 
-('Il était une fois la vie');
+    ('Lanfeust'),
+    ('Harry Potter'),
+    ('Lhistoire pour les nuls'),
+    ('LOTR'),
+    ('La vie est belle'),
+    ('La valse lente des tortues'),
+    ('Moi christiane F.'),
+    ('SQL pour les nuls'),
+    ('Il était une fois la vie');
 INSERT INTO patron (first_name, last_name)
-VALUES ('Franck','Michel'),
-('Jean','Valjean'),
-('Francis','Autant'),
-('Machin','Bidule'),
-('Jean','Coquetot'), 
-('Micheline', 'Dax'),
-('Robert', 'De niro'),
-('Hervé','Macheur'), 
-('Gerard', 'St Brice'),
-('Cunégonde', 'Mortefeuille'), 
-('Hector', 'Demarre');
+VALUES ('Franck', 'Michel'),
+    ('Jean', 'Valjean'),
+    ('Francis', 'Autant'),
+    ('Machin', 'Bidule'),
+    ('Jean', 'Coquetot'),
+    ('Micheline', 'Dax'),
+    ('Robert', 'De niro'),
+    ('Hervé', 'Macheur'),
+    ('Gerard', 'St Brice'),
+    ('Cunégonde', 'Mortefeuille'),
+    ('Hector', 'Demarre');
 -- try some borrows
 INSERT INTO borrow (id_patron, id_book, borrow_date)
-VALUES (1,1,NOW()),
-(2,3,NOW()),
-(3,2,NOW()),
-(3,4,NOW()),
-(5,2,NOW()),
-(5,1,NOW());
+VALUES (1, 1, NOW()),
+    (2, 3, NOW()),
+    (3, 2, NOW()),
+    (3, 4, NOW()),
+    (5, 2, NOW()),
+    (5, 1, NOW());
 -- test if a patron can borrow several book
 INSERT INTO borrow (id_patron, id_book, borrow_date)
 VALUES (1, 3, NOW());
@@ -73,15 +73,20 @@ SELECT COUNT(*)
 FROM borrow
 WHERE borrow.return_date IS NULL;
 -- is there un returned books?
-SELECT COUNT(*)>0
+SELECT COUNT(*) > 0
 FROM borrow
 WHERE borrow.return_date IS NULL;
 -- list of available books
-SELECT book.name as name
-, borrow.return_date
+SELECT book.name as name,
+    borrow.return_date
 FROM book
-LEFT JOIN borrow ON book.id=borrow.id_book
+    LEFT JOIN borrow ON book.id = borrow.id_book
 WHERE NOT borrow.return_date IS NULL;
 -- command to see all books borrowed
-SELECT DISTINCT first_name,last_name, book.name as book_name, borrow_date 
-FROM borrow NATURAL JOIN book NATURAL JOIN patron;
+SELECT DISTINCT first_name,
+    last_name,
+    book.name as book_name,
+    borrow_date
+FROM borrow
+    NATURAL JOIN book
+    NATURAL JOIN patron;
